fdf16e6c7dec602f51f8027cf97b5898a02e003b
Peter Salnikov <peter@salnikov.cc>
2017-12-25 03:10:22 +0300

    Version updated to 0.1.17

--- navio/builder/__init__.py
+++ navio/builder/__init__.py
@@ -2,7 +2,7 @@
 Lightweight Python Build Tool
 """
 
-__version__ = "0.1.16"
+__version__ = "0.1.17"
 __license__ = "MIT License"
 __website__ = "https://oss.navio.tech/navio-builder/"
 __download_url__ = 'https://github.com/naviotech/navio-builder/archive/{}.tar.gz'.format(__version__),
