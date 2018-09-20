182b702023a35c891387b2b3cbc11a5e03ed6935
Peter Salnikov <peter@salnikov.cc>
2018-09-20 12:05:44 +0300

    Version updated to 0.1.32

--- navio/meta_builder.py
+++ navio/meta_builder.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.31"
+__version__ = "0.1.32"
 __license__ = "MIT License"
 __website__ = "https://oss.navio.tech/navio-builder/"
 __download_url__ = ('https://github.com/naviotech/navio-builder/archive/'
